c62de5bbd987b2b11fe98db17769d59af9112175
sawantpras <sawantpras@gmail.com>
2017-03-28 23:52:25 +0530

    Add files via upload

---
+++ StringDemo.java
@@ -0,0 +1,14 @@
+public class StringDemo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		//declare one int variable
+		int a = 100;
+		//now declare string variable and assign int value to string variable using type casting
+		// here we used valueof method to do this
+		String s = String.valueOf(a);
+		System.out.println("The int converted to String is " + s);
+
+	}
+
+}
